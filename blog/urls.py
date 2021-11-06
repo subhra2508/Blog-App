012dfb8863bad8691e3da512da1288c8d8ad1e4f
subhrajyoti <subhrajyotisankarsau@gmail.com>
2021-11-06 21:41:29 +0530

    adding pagination
    apart from pagination i changed the function based view of the pagination to class based view,change the corresponding url

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-from blog.views import post_detail,post_list
+
+
+# from blog.views import post_list
+from blog.views import post_detail,PostListView
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('',post_list,name='post_list'),
+    # path('',post_list,name='post_list'),
+    path('',PostListView.as_view(),name="post_list"),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',post_detail,name='post_detail')
 ]
