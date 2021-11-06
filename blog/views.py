527fce430f9d5d4666c4165504dd49ca720ffb83
subhrajyoti <subhrajyotisankarsau@gmail.com>
2021-11-06 23:33:24 +0530

    implementing the email send feature

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,19 @@
-from django.shortcuts import render,get_object_or_404
-from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 
 
 from blog.models import Post
+from blog.forms import EmailPostForm
 
 
-# before pagination 
+# before pagination
 # def post_list(request):
 #     posts = Post.published.all()
 #     context = {'posts':posts}
 #     return render(request,'blog/post/list.html',context)
 
-#using pagination through function based view
+# using pagination through function based view
 
 # def post_list(request):
 #     object_list = Post.published.all();
@@ -33,7 +34,7 @@ from blog.models import Post
 #     return render(request,'blog/post/list.html',context)
 
 
-## using pagination through class based views
+# using pagination through class based views
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
@@ -41,14 +42,27 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-
-def post_detail(request,year,month,day,post):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
         slug=post,
         status='published',
-        publish__year = year,
-        publish__month = month,
-        publish__day = day
-        )
-    return render(request,'blog/post/detail.html',{'post':post})
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
+    )
+    return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {
+        'post': post,
+        'form': form
+    })
--- readme.md
+++ readme.md
@@ -1,3 +1,17 @@
 django gives you a free api to interact with them.You can find the data model reference of the official
 docs at :
 https://docs.djangoproject.com/en/3.0/ref/models/
+
+
+### enhancing your blog with advanced features
+
+- sharing posts via email
+- adding comments to a post
+- tagging posts
+- recommending similar posts
+
+### Here we can learn about
+- creating forms and handling them in views
+- creating forms from models 
+- integrating third-party applications
+- building complex QuerySets
